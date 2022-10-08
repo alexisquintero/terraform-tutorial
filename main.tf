25ded2d6fec64a04913debe65d65ea86055f0460
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2022-10-08 14:17:21 +0900

    Instance with vpc

--- main.tf
+++ main.tf
@@ -13,7 +13,30 @@ provider "aws" {
   profile = "terraform"
 }
 
-resource "aws_instance" "web" {
+resource "aws_vpc" "web_vpc" {
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "tuts vpc"
+  }
+}
+
+resource "aws_subnet" "web_subnet" {
+  vpc_id = aws_vpc.web_vpc.id
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "tuts subnet"
+  }
+}
+
+resource "aws_instance" "web_instance" {
   ami = "ami-017fecd1353bcc96e"
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.web_subnet.id
+
+  tags = {
+    Name = "tuts example"
+    foo = "bar"
+  }
 }
