449c8db521dad3eb01cbeac7b1848f94d95f6b30
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2022-10-10 12:00:28 +0900

    Outputs

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "instance_ip" {
+  value = aws_instance.web_instance.public_ip
+}
+
+output "instance_private_ip" {
+  value = aws_instance.web_instance.private_ip
+}
+
+output "vpc_data" {
+  value = aws_vpc.web_vpc
+  sensitive = true
+}
+
+output "output_value" {
+  value = "tuts"
+}
